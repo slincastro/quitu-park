d46dbd8b7a9f48d4a89d07fc86e220c4ebb7e19a
AluroX <aguilar.paulx@gmail.com>
2019-12-10 19:24:19 -0500

    add aplicattion QuituPark

---
+++ quitupark/src/main/java/com/quitulabs/quitupark/quitupark/QuituparkApplication.java
@@ -0,0 +1,13 @@
+package com.quitulabs.quitupark.quitupark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuituparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuituparkApplication.class, args);
+	}
+
+}
---
+++ quitupark/src/test/java/com/quitulabs/quitupark/quitupark/QuituparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quitulabs.quitupark.quitupark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuituparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
